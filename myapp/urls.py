f4a7fe66545b972816d3ba40e6f323c8179b607d
MoHAMeD AashRaF <151292203+mohamedasharf311@users.noreply.github.com>
2026-03-23 13:44:25 +0200

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -15,5 +15,9 @@ urlpatterns = [
     path('api/sync_data', views.api_sync_data, name='api_sync_data'),
     path('api/clear_session', views.api_clear_session, name='api_clear_session'),
     path('api/stats', views.api_stats, name='api_stats'),
+    path('api/sync_save', views.api_sync_save, name='api_sync_save'),
+    path('api/clear_data', views.api_clear_data, name='api_clear_data'),
+    path('api/debug', views.api_debug, name='api_debug'),
+
 # الصفحة 
 ]
