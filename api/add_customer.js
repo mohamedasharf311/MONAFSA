12ea9070bdde5357b95f84d3d581e9ac0b5a6603
MoHAMeD AashRaF <151292203+mohamedasharf311@users.noreply.github.com>
2026-03-23 13:05:21 +0200

    Delete api/add_customer.js

--- api/add_customer.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    if (req.method === 'POST') {
-        res.status(200).json({ success: true, queueNumber: 1 });
-    }
-}
